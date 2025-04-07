359044ca6e3d766e13ae13a32eee8623bf232ede
ludovicianul <iliemad@gmail.com>
2025-04-07 18:40:55 +0300

    chore: Update build info

--- src/main/java/io/ludovicianul/timi/version/VersionProvider.java
+++ src/main/java/io/ludovicianul/timi/version/VersionProvider.java
@@ -3,8 +3,8 @@ package io.ludovicianul.timi.version;
 import picocli.CommandLine;
 
 public class VersionProvider implements CommandLine.IVersionProvider {
-  public static final String VERSION = "1.0.0";
-  public static final String DATE = "2025-03-28";
+  public static final String VERSION = "1.0.3";
+  public static final String DATE = "2025-04-07";
 
   @Override
   public String[] getVersion() {
