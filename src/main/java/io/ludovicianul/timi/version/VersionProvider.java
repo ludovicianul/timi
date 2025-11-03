abada869a61d20da3397d77a1c92a89f798b12b8
ludovicianul <iliemad@gmail.com>
2025-11-03 09:03:11 +0200

    fix: Update version

--- src/main/java/io/ludovicianul/timi/version/VersionProvider.java
+++ src/main/java/io/ludovicianul/timi/version/VersionProvider.java
@@ -3,8 +3,8 @@ package io.ludovicianul.timi.version;
 import picocli.CommandLine;
 
 public class VersionProvider implements CommandLine.IVersionProvider {
-  public static final String VERSION = "1.0.5";
-  public static final String DATE = "2025-05-12";
+  public static final String VERSION = "1.0.7";
+  public static final String DATE = "2025-11-03";
 
   @Override
   public String[] getVersion() {
